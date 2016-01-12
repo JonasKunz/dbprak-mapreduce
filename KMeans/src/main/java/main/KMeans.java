c740536c2a95629a9c6e211ecfd45bc0405e99a4
Christopher Brecht <christopher.brecht1989@googlemail.com>
2016-01-12 15:11:34 +0100

    fix

--- KMeans/src/main/java/main/KMeans.java
+++ KMeans/src/main/java/main/KMeans.java
@@ -13,6 +13,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
@@ -50,9 +51,9 @@ public class KMeans {
 			
 			job = new Job(config);
 			job.setJobName(JOB_NAME);
-			job.setMapperClass(ClusterMapper.class);
+			job.setMapperClass(ClusterMapperNew.class);
 			job.setReducerClass(ClusterReducer.class);
-			job.setInputFormatClass(WordVectorInputFormat.class);
+			job.setInputFormatClass(TextInputFormat.class);
 			job.setOutputFormatClass(TextOutputFormat.class);
 			job.setOutputKeyClass(IntWritable.class);
 			job.setOutputValueClass(Vector.class);
